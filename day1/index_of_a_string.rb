de07bd8fd6221e8416bd7d6a3442291c5e85644b
Dan Kendall <dan@danstuken.com>
2011-06-13 18:53:06 +0100

    Added solution to finding index of 'Ruby' in 'Hello, Ruby'

---
+++ day1/index_of_a_string.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+#
+# Dan Kendall 06/2011
+#
+# index_of_a_string.rb
+
+source = 'Hello, Ruby'
+needle = 'Ruby'
+index = source.index(needle)
+
+puts "Index of \"#{needle}\" in \"#{source}\" is #{index}"
