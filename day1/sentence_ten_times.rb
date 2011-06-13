5b6078b87f6326fd101347b5d0e3ebbbd77d53ad
Dan Kendall <dan@danstuken.com>
2011-06-13 19:03:04 +0100

    Added solution to the updating sentence problem

---
+++ day1/sentence_ten_times.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+#
+#Dan Kendall 06/2011
+#
+#sentence_ten_times.rb
+#
+
+sentenceRoot = 'This is sentence number '
+printCount = 10
+
+(1..printCount).each do |i|
+   puts "#{sentenceRoot}#{i}"
+end
